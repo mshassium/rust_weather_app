36a6952f5b3ea59ed5342efa2f6f1a30181c1d6d
rakh1213 <rail.khamitov@netcracker.com>
2019-10-01 11:32:20 -0300

    add new url for daytime weather result

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,16 @@ extern crate rust_weather_cli;
 use std::io;
 use rust_weather_cli::utils;
 use serde_json;
+use whoami;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Hello {}, please chose what do you want?",whoami::username());
     let kelvin_value = 273.15;
     println!("Hello, please write city full name");
     let mut line = String::new();
     io::stdin().read_line(&mut line).expect("Not readable");
     println!("You city name is: {}\nPlease wait....",line);
-    let resp: String = reqwest::get(&utils::weather_url(line))?.text()?;
+    let resp: String = reqwest::get(&utils::current_weather_url(line))?.text()?;
     let resp_value: serde_json::value::Value = serde_json::from_str(&resp)?;
     let cod = resp_value.get("cod").unwrap();
     let final_temp_string = 
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,6 +1,11 @@
-pub fn weather_url(city : String) -> String {
+pub fn current_weather_url(city : String) -> String {
     let appid = "566e3e4fa2e15f8874dd5a8afb8af4d4";
     let url_raw = "https://api.openweathermap.org/data/2.5/weather?q={cityKey}&APPID={appidKey}";
     let result_url: String = url_raw.replace("{cityKey}",&city).replace("{appidKey}",appid);
+    return result_url;
+}
+
+pub fn daytime_weather_url(city: String, count_day: u32) -> String {
+    let result_url: String = current_weather_url(city) + &format!("&cnt={}",count_day);
     return result_url;
 }
