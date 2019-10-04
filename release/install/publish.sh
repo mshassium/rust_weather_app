60bd9c4d162a0abacb5fdfc0b1ca1c6a32572633
rakh1213 <rail.khamitov@netcracker.com>
2019-10-04 13:09:41 -0300

    publish new build

---
+++ release/install/publish.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo Publish start
+cargo build
+mv ../../target/debug/rust-weather-cli ./rweather
+git add .
+git commit -m "publish new build"
+git push origin master
+echo Publish finish
