25de601f136bc59565c82ddbb04f433d4a3d0989
rakh1213 <rail.khamitov@netcracker.com>
2019-10-04 13:16:07 -0300

    create publish script

--- release/install/install.sh
+++ release/install/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo Tobi Pizda
-wget -q https://github.com/mshassium/rust_weather_app/raw/master/release/v1.0/rust-weather-cli
-chmod u+x ./rust-weather-cli
-mv ./rust-weather-cli /usr/local/bin/rweather
+wget -q https://github.com/mshassium/rust_weather_app/raw/master/release/v1.0/rweather
+chmod u+x ./rweather
+mv ./rweather /usr/local/bin/rweather
 echo Запускаю епта.......
 echo команда rweather
 sleep 1s
--- release/install/publish.sh
+++ release/install/publish.sh
@@ -2,7 +2,7 @@
 echo Publish start
 cargo build
 mv ../../target/debug/rust-weather-cli ./rweather
-git add .
+git add ../../.
 git commit -m "publish new build"
 git push origin master
 echo Publish finish
