a4e70a34f44adfb7ded0367d76de69361ef195b1
rakh1213 <rail.khamitov@netcracker.com>
2019-10-05 11:23:30 -0300

    publish new build

--- release/install/install.sh
+++ release/install/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo Tobi Pizda
-wget -q https://github.com/mshassium/rust_weather_app/raw/master/release/v1.0/rweather
+wget -q https://github.com/mshassium/rust_weather_app/raw/master/release/v.0.0.1/rweather
 chmod u+x ./rweather
 mv ./rweather /usr/local/bin/rweather
 echo Запускаю епта.......
--- release/install/publish.sh
+++ release/install/publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo Publish start
 cargo build
-mv ../../target/debug/rweather ./rweather
+mv ../../target/debug/rweather ../v.0.0.1/rweather
 git add ../../.
 git commit -m "publish new build"
 git push origin master
