70a42b9d00526dcf66848519301859db9595e45a
rakh1213 <rail.khamitov@netcracker.com>
2019-10-05 11:53:16 -0300

    add clear sh for installation and publish (Only for MacOS and Unix)

--- release/install/install.sh
+++ release/install/install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-echo Tobi Pizda
-wget -q https://github.com/mshassium/rust_weather_app/raw/master/release/v.0.0.1/rweather
+echo Start installation
+wget https://github.com/mshassium/rust_weather_app/raw/master/release/v.0.0.1/rweather
 chmod u+x ./rweather
 mv ./rweather /usr/local/bin/rweather
-echo Запускаю епта.......
-echo команда rweather
-sleep 1s
-rweather
+echo Installation finish.......
+sleep 2s
+rweather -c Saratov
