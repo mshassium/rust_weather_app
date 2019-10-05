5d978da2b0e7fa15fa7a7dfeab11c4ef022acefa
rakh1213 <rail.khamitov@netcracker.com>
2019-10-05 13:19:09 -0300

    beautifier simple output

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,46 @@
+extern crate chrono;
 extern crate reqwest;
 extern crate rweather;
 #[macro_use]
 extern crate clap;
 
+use clap::App;
 use rweather::utils;
 use serde_json;
-use clap::App;
+use whoami;
+use chrono::{Utc};
 
 const KELVIN: f64 = 273.15;
 
 fn main() {
-        let yaml = load_yaml!("cli.yml");
-        let matches = App::from_yaml(yaml).get_matches();
-        let city: String = matches.value_of("city").unwrap_or("Default city 😁").to_string();
-        let day_count: u32 = matches.value_of("day_count").unwrap_or("0").parse().unwrap();
-        let result : String = match day_count {
-            0 | 1 => current_weather(city),
-            _ => day_time_weather()
-        };
-        println!("{}", result);
-    }
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+    let city: String = matches
+        .value_of("city")
+        .unwrap_or("Default city 😁")
+        .to_string();
+    let day_count: u32 = matches
+        .value_of("day_count")
+        .unwrap_or("0")
+        .parse()
+        .unwrap();
+    let result: String = match day_count {
+        0 | 1 => current_weather(city),
+        _ => day_time_weather(),
+    };
+    println!("{}", result);
+}
 
-fn day_time_weather() -> String{
+fn day_time_weather() -> String {
     String::from("😁 Ну типа я работаю, но как бы нет 😁")
 }
 
 fn current_weather(city_name: String) -> String {
+    println!(
+        "👋 Hi {}, 🕵 searching weather in{} 🏙️",
+        whoami::username(),
+        city_name
+    );
     let resp: String = reqwest::get(&utils::current_weather_url(&city_name))
         .unwrap()
         .text()
@@ -34,15 +49,16 @@ fn current_weather(city_name: String) -> String {
     let cod = resp_value.get("cod").unwrap();
     let final_temp_string = if cod == 200 {
         format!(
-            "{}°C in {}",
+            "📅 {} \n⭐ In{}: {}°C",
+            Utc::now().format("%d.%m.%Y"),
+            &city_name,
             resp_value
                 .get("main")
                 .and_then(|value| value.get("temp"))
                 .and_then(|value| value.as_f64())
                 .and_then(|value| Some((value - KELVIN).round()))
                 .and_then(|value| Some(value.to_string()))
-                .unwrap(),
-            &city_name
+                .unwrap()
         )
     } else {
         format!(
